ffdd78ab412676e29c5eedeb7a4e1556dcb73c4e
skydzhugashvili1 <skyxgamiing+render@gmail.com>
2024-06-16 18:34:25 -0400

    Update and rename start3.sh to start4.sh

--- start4.sh
+++ start4.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-# Ensure jq is installed
-if ! command -v jq &> /dev/null
-then
-    echo "jq could not be found, please install jq."
-    exit 1
-fi
+# Function to install a package using apt
+install_package() {
+    PACKAGE_NAME=$1
+    if ! dpkg -l | grep -q "^ii  $PACKAGE_NAME"; then
+        echo "Installing $PACKAGE_NAME..."
+        sudo apt-get update
+        sudo apt-get install -y $PACKAGE_NAME
+        if [ $? -ne 0 ]; then
+            echo "Failed to install $PACKAGE_NAME. Exiting..."
+            exit 1
+        fi
+    else
+        echo "$PACKAGE_NAME is already installed."
+    fi
+}
 
-# Install ngrok
+# Ensure curl is installed
+install_package curl
+
+# Ensure jq is installed
+install_package jq
+
+# Install ngrok using apt
 echo "Installing ngrok..."
 curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
     && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list \
@@ -15,8 +30,7 @@ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trust
     && sudo apt install -y ngrok
 
 # Check if ngrok was installed successfully
-if ! command -v ngrok &> /dev/null
-then
+if ! command -v ngrok &> /dev/null; then
     echo "ngrok could not be installed, please check your network connection and try again."
     exit 1
 fi
@@ -25,17 +39,17 @@ fi
 VERSION_MANIFEST_URL="https://launchermeta.mojang.com/mc/game/version_manifest.json"
 
 # Fetch the latest version manifest and extract the URL for the latest release
-MC_SERVER_URL=$(wget -qO- $VERSION_MANIFEST_URL | jq -r '.latest.release as $version | .versions[] | select(.id == $version) | .url')
+MC_SERVER_URL=$(curl -s $VERSION_MANIFEST_URL | jq -r '.latest.release as $version | .versions[] | select(.id == $version) | .url')
 
 # Fetch the download URL for the server JAR from the extracted URL
-MC_SERVER_JAR_URL=$(wget -qO- $MC_SERVER_URL | jq -r '.downloads.server.url')
+MC_SERVER_JAR_URL=$(curl -s $MC_SERVER_URL | jq -r '.downloads.server.url')
 
 # Define the filename for the Minecraft server JAR
 MC_SERVER_JAR="minecraft_server.jar"
 
 # Download the latest Minecraft server JAR
 echo "Downloading the latest Minecraft server JAR from $MC_SERVER_JAR_URL..."
-wget -O $MC_SERVER_JAR $MC_SERVER_JAR_URL
+curl -o $MC_SERVER_JAR $MC_SERVER_JAR_URL
 
 # Check if the download was successful
 if [ $? -ne 0 ]; then
