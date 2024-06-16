a86a87293c1f7400a57f717c24ffd5229bf2e23a
skydzhugashvili1 <skyxgamiing+render@gmail.com>
2024-06-16 18:47:07 -0400

    Update and rename startfinalhopefullye.sh to startfinalhopefullye2.sh

--- startfinalhopefullye2.sh
+++ startfinalhopefullye2.sh
@@ -24,7 +24,7 @@ install_package gnupg
 # Install Java
 echo "Installing the latest version of Java..."
 sudo apt-get update
-sudo apt-get install -y openjdk-22-jdk
+sudo apt-get install -y openjdk-21-jdk
 
 # Verify Java installation
 if ! command -v java &> /dev/null
